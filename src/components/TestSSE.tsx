83bf2083916e3ab91d6f19a2d14bd13abee5fdc6
Kasper Mróz <kaspermroz1234@gmail.com>
2022-05-06 18:38:37 +0200

    Create test SSE connection

---
+++ src/components/TestSSE.tsx
@@ -0,0 +1,24 @@
+import {useEffect} from "react";
+
+export const TestSSE = () => {
+    useEffect(() => {
+        const sse = new EventSource('http://localhost:8080/api/v1/test',
+            { withCredentials: false });
+        function getRealtimeData(data: any) {
+            console.log(data)
+        }
+        sse.onopen = () => console.log("SSE conn open")
+        sse.onmessage = e => getRealtimeData(JSON.parse(e.data))
+        sse.onerror = (err) => {
+            console.error(err)
+
+            sse.close();
+        }
+        return () => {
+            sse.close();
+        };
+    }, []);
+    return (<span>
+        Test SSE is mounted - check console.
+    </span>)
+}
